f8d0bfce362cade6735f34abaef572b0657101cd
Jon Norman <jon.norman@depop.com>
2023-06-17 14:08:03 +0100

    Wip: fetch projects from the DB.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import landingImage from './assets/images/landing-map.png';
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
-import { Database } from './database.types'
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { Database } from './database.types';
 
 const Banner: React.FC = () => {
   return (
@@ -71,41 +71,43 @@ function shipmentsPath() {
   return '';
 }
 
-async function getProjects(supabase: SupabaseClient) {
-
-  const { data, error } = await supabase
-  .from('project')
-  .select()
+async function getProjects(supabase: SupabaseClient<Database>) {
+  const { data, error } = await supabase.from('project').select('*');
 
   if (error) {
-    console.error('Found an issue fetching projects.', error)
+    console.error('Found an issue fetching projects.', error);
+    return Promise.resolve([]);
   }
 
-  return data
+  return data;
 }
 
 const App: React.FC = () => {
-
-/*
+  /*
     TODO these should be passed in via the env/env file, one for local and one for production.
 
     Where/how should we do this?
   */
   const supabaseUrl = 'http://localhost:54321';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
+  const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
   const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
-  getProjects(supabase).then(console.log);
+  const projects = getProjects(supabase);
+
+  projects.then(console.log);
 
   return (
     <>
       <Banner />
-      <ProjectsList projects={[
-    { name: 'sierra leone' },
-    { name: 'lebanon' },
-    { name: 'ukraine' },
-    { name: 'turkey' },
-  ]} />
+      <ProjectsList
+        projects={[
+          { name: 'sierra leone' },
+          { name: 'lebanon' },
+          { name: 'ukraine' },
+          { name: 'turkey' },
+        ]}
+      />
       <Footer />
     </>
   );
