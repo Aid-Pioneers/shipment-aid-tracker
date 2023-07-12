ddcd44290d68839c3294c531260621894fe3be31
killerdaisies <janegongxw@gmail.com>
2023-07-12 16:03:49 +0800

    feat: constants file to store keys

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,6 @@ async function getProjects(supabase: SupabaseClient<Database>) {
 }
 
 const App: React.FC = () => {
-  /*
-    TODO these should be passed in via the env/env file, one for local and one for production.
-
-    Where/how should we do this?
-  */
-  const supabaseUrl = 'http://localhost:54321';
-  const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
   const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
   const projects = getProjects(supabase);
