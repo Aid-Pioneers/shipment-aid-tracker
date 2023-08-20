050aacff8d69f518846769e0c7d44a6c48bfa5dd
Jon Norman <jon.norman@depop.com>
2023-08-20 18:32:55 +0100

    fix: allow empty dependency array for static function.

--- src/containers/project-overview/index.tsx
+++ src/containers/project-overview/index.tsx
@@ -38,6 +38,7 @@ export const ProjectOverview: React.FC<ProjectOverviewContainerProps> = ({ supab
     }
 
     loadProjects();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
