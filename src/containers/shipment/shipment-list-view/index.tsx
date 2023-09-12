335cbb1fcde7f7902e04538ab4b5cbf37cd2f154
killerdaisies <janegongxw@gmail.com>
2023-09-12 09:29:24 +0100

    feat: shipment container and service skeleton

--- src/containers/shipment/shipment-list-view/index.tsx
+++ src/containers/shipment/shipment-list-view/index.tsx
@@ -1,33 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { ShipmentService } from '../../../services/shipment-service';
-import { DbShipment } from '../../../types/aliases';
 
 interface ShipmentOverviewProps {
   shipmentService: ShipmentService;
 }
 
 export const ShipmentOverview: React.FC<ShipmentOverviewProps> = ({ shipmentService }) => {
-  const [shipments, setShipments] = useState<DbShipment[]>([]);
+  // TODO update types for useState
+  const [shipments, setShipments] = useState<any>([]);
   const [errors, setErrors] = useState<String[]>([]);
 
   useEffect(() => {
     const loadShipments = async () => {
-      const { data, error } = await shipmentService.fetchShipments();
-      if (data) setShipments(data);
-      if (error) setErrors([error.message]);
+      return shipmentService.fetchShipments(
+        (data) => setShipments(data),
+        (error) => setErrors([error.message])
+      );
     };
     loadShipments();
   }, [shipmentService]);
 
   return (
     <>
-      {errors.length > 0 ? (
-        // TODO #15 make an error component and pass error in as props
-        errors.map((error) => <p>{error}</p>)
-      ) : (
-        // TODO: shipment list component
-        <p>{JSON.stringify(shipments)}</p>
-      )}
+      {errors.length > 0
+        ? // TODO #15 make an error component and pass error in as props
+          errors.map((error) => <p>{error}</p>)
+        : // TODO: shipment list component
+          { shipments }}
     </>
   );
 };
