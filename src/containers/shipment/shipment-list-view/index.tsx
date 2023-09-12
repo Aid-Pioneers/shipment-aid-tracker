e5e76440ac7bca0c32e451f945f8913d37ba340e
killerdaisies <janegongxw@gmail.com>
2023-09-12 09:29:24 +0100

    feat: shipment container and service skeleton

---
+++ src/containers/shipment/shipment-list-view/index.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { ShipmentService } from '../../../services/shipment-service';
+
+interface ShipmentOverviewProps {
+  shipmentService: ShipmentService;
+}
+
+export const ShipmentOverview: React.FC<ShipmentOverviewProps> = ({ shipmentService }) => {
+  // TODO update types for useState
+  const [shipments, setShipments] = useState<any>([]);
+  const [errors, setErrors] = useState<String[]>([]);
+
+  useEffect(() => {
+    const loadShipments = async () => {
+      return shipmentService.fetchShipments(
+        (data) => setShipments(data),
+        (error) => setErrors([error.message])
+      );
+    };
+    loadShipments();
+  }, [shipmentService]);
+
+  return (
+    <>
+      {errors.length > 0
+        ? // TODO #15 make an error component and pass error in as props
+          errors.map((error) => <p>{error}</p>)
+        : // TODO: shipment list component
+          { shipments }}
+    </>
+  );
+};
--- src/types/aliases.ts
+++ src/types/aliases.ts
@@ -1,3 +1,5 @@
 import { Database } from "../database.types";
 
 export type DbProject = Database['public']['Tables']['project']['Row']
+
+export type DbShipment = Database['public']['Tables']['shipment']['Row']
