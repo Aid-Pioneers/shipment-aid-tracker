7a4021745ee6b4476bc482b095530d318c9c7103
killerdaisies <janegongxw@gmail.com>
2023-09-14 17:21:59 +0100

    shipment get service bug fix and shipment creation container

---
+++ src/containers/shipment/shipment-creation/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { ShipmentService } from '../../../services/shipment-service';
+
+interface ShipmentCreationContainerProps {
+  shipmentService: ShipmentService;
+}
+
+export const ShipmentCreationContainer: React.FC<ShipmentCreationContainerProps> = ({ shipmentService }) => {
+  // TODO update types for useState
+
+  return <></>;
+};
