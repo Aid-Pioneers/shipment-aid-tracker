d21e536bdb29df40d6587c1e7240eb8eee4df8bb
killerdaisies <janegongxw@gmail.com>
2023-10-04 00:40:14 +0100

    feat: styling of form and more input fields

--- src/containers/shipment/shipment-creation/index.tsx
+++ src/containers/shipment/shipment-creation/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ShipmentService } from '../../../services/shipment-service';
 import { useForm } from 'react-hook-form';
+import { FormItemWrapper, FormWrapper } from './index.styles';
 
 interface ShipmentCreationContainerProps {
   shipmentService: ShipmentService;
@@ -9,24 +10,50 @@ interface ShipmentCreationContainerProps {
 export const ShipmentCreationContainer: React.FC<ShipmentCreationContainerProps> = ({ shipmentService }) => {
   const { handleSubmit } = useForm<FormData>();
 
+  console.log({ shipmentService });
   const onSubmit = () => {};
   return (
-    <>
+    <FormWrapper>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
+        <FormItemWrapper>
           <label>Starting Location:</label>
           <input name="starting_location" />
           {/* {errors.starting_location && <span>This field is required</span>} */}
-        </div>
+        </FormItemWrapper>
 
-        <div>
+        <FormItemWrapper>
           <label>Destination Location:</label>
-          <input name="destination_location" />
+          {/* TODO this needs to be a dropdown */}
           {/* {errors.destination_location && <span>This field is required</span>} */}
-        </div>
+        </FormItemWrapper>
+        <FormItemWrapper>
+          <label>Cure ID</label>
+          <input name="cure_id" />
+        </FormItemWrapper>
+        <FormItemWrapper>
+          <label>Shipping ID</label>
+          <input name="shipping_id" />
+        </FormItemWrapper>
+        <FormItemWrapper>
+          <label>Delivery Date</label>
+          <input name="delivery_date" />
+        </FormItemWrapper>
+        <FormItemWrapper>
+          <label>Recipient</label>
+          {/* TODO dropdown?? */}
+        </FormItemWrapper>
+        <FormItemWrapper>
+          <label>Weight</label>
+          <input name="shipment_weight" />
+        </FormItemWrapper>
+
+        <FormItemWrapper>
+          <label>Value</label>
+          <input name="shipment_value" />
+        </FormItemWrapper>
 
         <button type="submit">Create Shipment</button>
       </form>
-    </>
+    </FormWrapper>
   );
 };
