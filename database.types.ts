eee0278a69894294a783118728a86532342729b9
Jon Norman <jon.norman@depop.com>
2023-10-21 10:50:51 +0100

    Clean up migrations and remove unnecessary statements.

--- database.types.ts
+++ database.types.ts
@@ -9,6 +9,63 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      consignee_partner: {
+        Row: {
+          created_at: string | null
+          id: number
+          name: string
+          updated_at: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          name: string
+          updated_at?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          name?: string
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
+      country: {
+        Row: {
+          id: number
+          name: string
+        }
+        Insert: {
+          id?: number
+          name: string
+        }
+        Update: {
+          id?: number
+          name?: string
+        }
+        Relationships: []
+      }
+      donor: {
+        Row: {
+          created_at: string | null
+          id: number
+          name: string
+          updated_at: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id?: number
+          name: string
+          updated_at?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          name?: string
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
       pallet: {
         Row: {
           created_at: string
@@ -84,31 +141,142 @@ export interface Database {
       }
       shipment: {
         Row: {
+          bookkeeping_flexport: boolean | null
+          bookkeeping_gik: boolean | null
+          commercial_invoice_link: string | null
+          consignee: number | null
           created_at: string
+          destination_id: number
+          donor_id: number | null
+          drive_link: string | null
+          estimated_delivery_date: string | null
+          flexport_tracking_id: string | null
+          freight_tracking_id: string | null
+          gik_value_amount_usd: number | null
           id: string
+          impact_reporting: boolean | null
+          logistics_expense_donation_usd: number | null
+          main_shipment_type_id: number | null
+          patients_treated: number | null
           project_id: string
+          status: number
           updated_at: string
+          weight_kg: number | null
         }
         Insert: {
+          bookkeeping_flexport?: boolean | null
+          bookkeeping_gik?: boolean | null
+          commercial_invoice_link?: string | null
+          consignee?: number | null
           created_at?: string
+          destination_id: number
+          donor_id?: number | null
+          drive_link?: string | null
+          estimated_delivery_date?: string | null
+          flexport_tracking_id?: string | null
+          freight_tracking_id?: string | null
+          gik_value_amount_usd?: number | null
           id?: string
+          impact_reporting?: boolean | null
+          logistics_expense_donation_usd?: number | null
+          main_shipment_type_id?: number | null
+          patients_treated?: number | null
           project_id: string
+          status: number
           updated_at?: string
+          weight_kg?: number | null
         }
         Update: {
+          bookkeeping_flexport?: boolean | null
+          bookkeeping_gik?: boolean | null
+          commercial_invoice_link?: string | null
+          consignee?: number | null
           created_at?: string
+          destination_id?: number
+          donor_id?: number | null
+          drive_link?: string | null
+          estimated_delivery_date?: string | null
+          flexport_tracking_id?: string | null
+          freight_tracking_id?: string | null
+          gik_value_amount_usd?: number | null
           id?: string
+          impact_reporting?: boolean | null
+          logistics_expense_donation_usd?: number | null
+          main_shipment_type_id?: number | null
+          patients_treated?: number | null
           project_id?: string
+          status?: number
           updated_at?: string
+          weight_kg?: number | null
         }
         Relationships: [
+          {
+            foreignKeyName: "shipment_consignee_fkey"
+            columns: ["consignee"]
+            referencedRelation: "consignee_partner"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "shipment_destination_id_fkey"
+            columns: ["destination_id"]
+            referencedRelation: "country"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "shipment_donor_id_fkey"
+            columns: ["donor_id"]
+            referencedRelation: "donor"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "shipment_main_shipment_type_id_fkey"
+            columns: ["main_shipment_type_id"]
+            referencedRelation: "shipment_type"
+            referencedColumns: ["id"]
+          },
           {
             foreignKeyName: "shipment_project_id_fkey"
             columns: ["project_id"]
             referencedRelation: "project"
             referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "shipment_status_fkey"
+            columns: ["status"]
+            referencedRelation: "shipment_status"
+            referencedColumns: ["id"]
           }
         ]
+      }
+      shipment_status: {
+        Row: {
+          id: number
+          status: string
+        }
+        Insert: {
+          id?: number
+          status: string
+        }
+        Update: {
+          id?: number
+          status?: string
+        }
+        Relationships: []
+      }
+      shipment_type: {
+        Row: {
+          id: number
+          shipment_type: string
+        }
+        Insert: {
+          id?: number
+          shipment_type: string
+        }
+        Update: {
+          id?: number
+          shipment_type?: string
+        }
+        Relationships: []
       }
     }
     Views: {
@@ -125,3 +293,4 @@ export interface Database {
     }
   }
 }
+
