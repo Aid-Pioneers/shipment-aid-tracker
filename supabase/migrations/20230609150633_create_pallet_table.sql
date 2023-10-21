db028b8208c12d87a1ae322eda92138e74f5be53
Jon Norman <jon.norman@depop.com>
2023-10-21 11:48:31 +0100

    Reset all of the migrations.

--- supabase/migrations/20230609150633_create_pallet_table.sql
+++
@@ -1,8 +0,0 @@
-create table
-  public.pallet (
-    id uuid default uuid_generate_v4() not null,
-    created_at timestamp with time zone not null default (now() at time zone 'utc'::text),
-    updated_at timestamp with time zone not null default (now() at time zone 'utc'::text),
-    shipment_id uuid not null,
-    constraint pallet_pkey primary key (id)
-  ) tablespace pg_default;
