dd33b80718d17d34527dd6bbd3de4d58505084f3
Jon Norman <jon.norman@depop.com>
2023-12-16 00:52:52 +0000

    Try using the accordion type instead of own collapsible.

--- src/components/shipment/creation/impact/index.tsx
+++ src/components/shipment/creation/impact/index.tsx
@@ -1,29 +1,19 @@
-import { Collapse } from '@chakra-ui/react';
 import * as React from 'react';
 import { useForm } from 'react-hook-form';
-import { FormWrapper } from '../../../../containers/shipment/shipment-creation/index.styles';
-import { CollapsibleFormHeaderComponent } from '../../../collapsible-form-header';
 
 interface ImpactComponentProps {
-  startCollapsed?: boolean
 }
 
-export const ShipmentCreationImpactComponent: React.FC<ImpactComponentProps> = ({ startCollapsed }) => {
+export const ShipmentCreationImpactComponent: React.FC<ImpactComponentProps> = () => {
 
   const { handleSubmit } = useForm<FormData>()
 
-  const [isCollapsed, setCollapsed] = React.useState<boolean | undefined>(startCollapsed);
 
   const onSubmit = () => { };
 
   return (
-    <FormWrapper>
-      <CollapsibleFormHeaderComponent header="Impact" isCollapsed={isCollapsed} setCollapsed={setCollapsed} />
-      <Collapse in={typeof isCollapsed === 'undefined' || !isCollapsed}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          {/* TODO what goes in this section? */}
-        </form>
-      </Collapse>
-    </FormWrapper>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* TODO what goes in this section? */}
+    </form>
   );
 };
--- src/containers/shipment/shipment-creation/index.tsx
+++ src/containers/shipment/shipment-creation/index.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Heading, VStack } from '@chakra-ui/react';
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, HStack, Heading } from '@chakra-ui/react';
 import React from 'react';
 import { ShipmentCreationFinanceComponent } from '../../../components/shipment/creation/finance';
 import { ShipmentCreationGeneralComponent } from '../../../components/shipment/creation/general';
@@ -6,6 +6,7 @@ import { ShipmentCreationImpactComponent } from '../../../components/shipment/cr
 import { ShipmentCreationPackingListComponent } from '../../../components/shipment/creation/packing-list';
 import { ShipmentCreationTrackingComponent } from '../../../components/shipment/creation/tracking';
 import { ShipmentService } from '../../../services/shipment-service';
+import { FormWrapper } from './index.styles';
 interface ShipmentCreationContainerProps {
   shipmentService: ShipmentService;
 }
@@ -15,14 +16,88 @@ export const ShipmentCreationContainer: React.FC<ShipmentCreationContainerProps>
   // NB: this container will handle making API calls and passing props into
   // Create a React context with all the data from the API required to render the forms (donors, destinations, statuses etc)
   return (
-    <VStack>
-      {/* TODO can we make this h1 left aligned? */}
+    <Accordion defaultIndex={[0]} allowMultiple={true}>
       <Heading as="h1">Create a shipment</Heading>
-      <ShipmentCreationGeneralComponent />
-      <ShipmentCreationPackingListComponent startCollapsed={false} />
-      <ShipmentCreationTrackingComponent startCollapsed={true} />
-      <ShipmentCreationFinanceComponent startCollapsed={true} />
-      <ShipmentCreationImpactComponent startCollapsed={true} />
+      <FormWrapper>
+        <AccordionItem>
+          <h1>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                General
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h1>
+          <AccordionPanel pb={0}>
+            <ShipmentCreationGeneralComponent />
+          </AccordionPanel>
+        </AccordionItem>
+      </FormWrapper>
+
+      <FormWrapper>
+        <AccordionItem>
+          <h1>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                Packing List
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h1>
+          <AccordionPanel pb={0}>
+            <ShipmentCreationPackingListComponent />
+          </AccordionPanel>
+        </AccordionItem>
+      </FormWrapper>
+
+      <FormWrapper>
+        <AccordionItem>
+          <h1>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                Tracking
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h1>
+          <AccordionPanel pb={0}>
+            <ShipmentCreationTrackingComponent />
+          </AccordionPanel>
+        </AccordionItem>
+      </FormWrapper>
+
+      <FormWrapper>
+        <AccordionItem>
+          <h1>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                Finance
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h1>
+          <AccordionPanel pb={0}>
+            <ShipmentCreationFinanceComponent />
+          </AccordionPanel>
+        </AccordionItem>
+      </FormWrapper>
+
+      <FormWrapper>
+        <AccordionItem>
+          <h1>
+            <AccordionButton>
+              <Box as="span" flex='1' textAlign='left'>
+                Impact
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h1>
+          <AccordionPanel pb={0}>
+            <ShipmentCreationImpactComponent />
+          </AccordionPanel>
+
+        </AccordionItem>
+      </FormWrapper>
       {/* TODO can we make this HStack left aligned? */}
       <HStack spacing={20} align="left">
         <Button colorScheme="blue" variant="outline">
@@ -32,6 +107,6 @@ export const ShipmentCreationContainer: React.FC<ShipmentCreationContainerProps>
           Save
         </Button>
       </HStack>
-    </VStack>
+    </Accordion>
   );
 };
