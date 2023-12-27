9cd18d222bfdcfab07aa1c7037c906747a17d25c
Jon Norman <jon.norman@depop.com>
2023-12-27 10:29:23 +0000

    Remove unused components for now - they were just design placeholders.

--- src/containers/shipment/shipment-creation/index.tsx
+++ src/containers/shipment/shipment-creation/index.tsx
@@ -1,8 +1,6 @@
 import { Button, HStack, Heading, VStack } from '@chakra-ui/react';
 import React from 'react';
-import { ShipmentCreationFinanceComponent } from '../../../components/shipment/creation/finance';
 import { ShipmentCreationGeneralComponent } from '../../../components/shipment/creation/general';
-import { ShipmentCreationImpactComponent } from '../../../components/shipment/creation/impact';
 import { ShipmentCreationPackingListComponent } from '../../../components/shipment/creation/packing-list';
 import { ShipmentCreationTrackingComponent } from '../../../components/shipment/creation/tracking';
 import { ShipmentService } from '../../../services/shipment-service';
@@ -21,8 +19,6 @@ export const ShipmentCreationContainer: React.FC<ShipmentCreationContainerProps>
       <ShipmentCreationGeneralComponent />
       <ShipmentCreationPackingListComponent startCollapsed={false} />
       <ShipmentCreationTrackingComponent startCollapsed={true} />
-      <ShipmentCreationFinanceComponent startCollapsed={true} />
-      <ShipmentCreationImpactComponent startCollapsed={true} />
       {/* TODO can we make this HStack left aligned? */}
       <HStack spacing={20} align="left">
         <Button colorScheme="blue" variant="outline">
