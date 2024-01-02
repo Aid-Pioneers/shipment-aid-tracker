e42fc6012d120286c0c46604e969e20a1c6a405b
Jon Norman <jon.norman@depop.com>
2024-01-02 09:35:52 +0000

    Add origin field to shipment table.

--- supabase/migrations/20231228101952_create-profiles-and-profile-roles.sql
+++ supabase/migrations/20231228101952_create-profiles-and-profile-roles.sql
@@ -147,11 +147,3 @@ $$;
 create trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user();
-
--- add an origin for a shipment
-alter table public.shipment
-add column "origin_id" bigint not null;
-
-alter table "public"."shipment" add constraint "shipment_origin_id_fkey" FOREIGN KEY (origin_id) REFERENCES country(id) not valid;
-
-alter table "public"."shipment" validate constraint "shipment_origin_id_fkey";
