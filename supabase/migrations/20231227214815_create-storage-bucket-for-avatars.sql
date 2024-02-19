df80e01d33cfed2771be7fe16673a6d9c5751846
Jon Norman <jon.norman@depop.com>
2024-02-19 10:05:02 +0000

    Do not error if storage bucket already exists.

--- supabase/migrations/20231227214815_create-storage-bucket-for-avatars.sql
+++ supabase/migrations/20231227214815_create-storage-bucket-for-avatars.sql
@@ -1,4 +1,5 @@
 insert into storage.buckets
   (id, name, public, file_size_limit, allowed_mime_types)
 values
-  ('avatars', 'avatars', false, 2097152, '{"image/jpeg", "image/png"}');
+  ('avatars', 'avatars', false, 2097152, '{"image/jpeg", "image/png"}')
+on conflict do nothing;
