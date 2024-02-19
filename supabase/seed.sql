05f9e0447a6c11a4537ff4463578a536b4ac2954
Jon Norman <jon.norman@depop.com>
2024-02-19 09:40:08 +0000

    Update all auth and identities to match an organic sign-up.

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -21,37 +21,6 @@ values
     ('Sierra Leone');
 
 insert into
-    auth.users (
-        instance_id
-        ,id
-        ,aud
-        ,role
-        ,email
-        ,encrypted_password
-        ,confirmation_token
-        ,confirmation_sent_at
-        ,raw_app_meta_data
-        ,raw_user_meta_data
-        ,created_at
-        ,updated_at
-        )
-values
-    (
-        '00000000-0000-0000-0000-000000000000'::uuid
-        ,'ac2fbd37-6f6b-469b-8ea9-72ebbd91cbb8'::uuid
-        ,'authenticated'
-        ,'authenticated'
-        ,'test.user@aidpioneers.com'
-        ,'$2a$10$3sUm3VHgi2j/BWZbHmBTJeZ7hLkd9SHdJlmWWv30U8GgiCJwQ4ram' /* Password123 */
-        ,'811c1ec831131e9d56e6bd83546cd6e140080596cae1f2381087d701'
-        ,'2023-12-28 19:53:53.701273+00'
-        ,'{"provider": "email", "providers": ["email"]}'
-        ,'{"first_name": "Mae","last_name": "Martin"}'
-        ,now()
-        ,now()
-    );
-
-insert into
     public.shipment (id, project_id, destination_id, origin_id, donor_id, status_id)
 values
     (1, 1, 238,235, 1, 1);
@@ -60,3 +29,150 @@ insert into
     public.pallet (id, shipment_id)
 values
     (1, 1);
+
+-- seed two users:
+-- jon.doe@aidpioneers - a donor
+-- jane.doe@aidpioneers - a manager
+
+insert into auth.users (
+    instance_id
+    ,id
+    ,aud
+    ,role
+    ,email
+    ,encrypted_password
+    ,email_confirmed_at
+    ,invited_at
+    ,confirmation_token
+    ,confirmation_sent_at
+    ,recovery_token
+    ,recovery_sent_at
+    ,email_change_token_new
+    ,email_change
+    ,email_change_sent_at
+    ,last_sign_in_at
+    ,raw_app_meta_data
+    ,raw_user_meta_data
+    ,is_super_admin
+    ,created_at
+    ,updated_at
+    ,phone
+    ,phone_confirmed_at
+    ,phone_change
+    ,phone_change_token
+    ,phone_change_sent_at
+    ,email_change_token_current
+    ,email_change_confirm_status
+    ,banned_until
+    ,reauthentication_token
+    ,reauthentication_sent_at
+    ,is_sso_user
+    ,deleted_at
+) values (
+    '00000000-0000-0000-0000-000000000000'
+    ,'9f28f76e-e7cb-48ef-9838-8075d4fed06f'
+    ,'authenticated'
+    ,'authenticated'
+    ,'jon.doe@aidpioneers.com'
+    ,'$2a$10$N/ezF2EMzLsZ5tPfmNm/7e.pCozFiaPm9g0THHboKyw.mSpO3HUqy' /* Password123 */
+    ,current_timestamp
+    ,null
+    ,''
+    ,null
+    ,''
+    ,null
+    ,''
+    ,''
+    ,null
+    ,current_timestamp
+    ,'{"provider": "email" ,"providers": ["email"]}'
+    ,'{"last_name": "Jon","first_name": "Doe"}'
+    ,null
+    ,current_timestamp
+    ,current_timestamp
+    ,null
+    ,null
+    ,''
+    ,''
+    ,null
+    ,''
+    ,0
+    ,null
+    ,''
+    ,null
+    ,false
+    ,null
+    ),
+    (
+    '00000000-0000-0000-0000-000000000000'
+    ,'225f5db4-9551-4ba2-9638-9f94745b4007'
+    ,'authenticated'
+    ,'authenticated'
+    ,'jane.doe@aidpioneers.com'
+    ,'$2a$10$gwa3Od2m/QMcJ1PcgZZfC.5Te/fiN.6tmgvcvRBPUfaDA0GYZslR.' /* Password123 */
+    ,current_timestamp
+    ,null
+    ,''
+    ,null
+    ,''
+    ,null
+    ,''
+    ,''
+    ,null
+    ,current_timestamp
+    ,'{"provider": "email" ,"providers": ["email"]}'
+    ,'{"last_name": "Jane","first_name": "Doe"}'
+    ,null
+    ,current_timestamp
+    ,current_timestamp
+    ,null
+    ,null
+    ,''
+    ,''
+    ,null
+    ,''
+    ,0
+    ,null
+    ,''
+    ,null
+    ,false
+    ,null
+    )
+    ;
+
+insert into auth.identities (
+    provider_id
+    ,user_id
+    ,identity_data
+    ,provider
+    ,last_sign_in_at
+    ,created_at
+    ,updated_at
+    ,id
+) values (
+    '9f28f76e-e7cb-48ef-9838-8075d4fed06f'
+    ,'9f28f76e-e7cb-48ef-9838-8075d4fed06f'
+    ,'{"sub": "9f28f76e-e7cb-48ef-9838-8075d4fed06f", "email": "jon.doe@aidpioneers.com", "email_verified": false, "phone_verified": false}'
+    ,'email'
+    ,current_timestamp
+    ,current_timestamp
+    ,current_timestamp
+    ,'84efa341-3a92-4d0f-9106-820bfb9f20a1'
+    ),
+    (
+    '225f5db4-9551-4ba2-9638-9f94745b4007'
+    ,'225f5db4-9551-4ba2-9638-9f94745b4007'
+    ,'{"sub": "225f5db4-9551-4ba2-9638-9f94745b4007", "email": "jane.doe@aidpioneers.com", "email_verified": false, "phone_verified": false}'
+    ,'email'
+    ,current_timestamp
+    ,current_timestamp
+    ,current_timestamp
+    ,'d7ac68ed-3651-41d8-a3e7-9f273f8b091b'
+    );
+
+insert into public.profile_role (
+    profile_id
+    ,role_id
+) values
+    ('9f28f76e-e7cb-48ef-9838-8075d4fed06f', 2)
+    ,('225f5db4-9551-4ba2-9638-9f94745b4007', 1);
