140abdf5a59e05b5acc4bd5eda389fd4f9b80f98
Jon Norman <jon.norman@depop.com>
2024-06-17 10:53:40 +0100

    Render the shipment status card.

--- src/containers/shipment/shipment-view-page/index.tsx
+++ src/containers/shipment/shipment-view-page/index.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ShipmentService } from '../../../services/shipment-service';
-import { DbShipment } from '../../../types/aliases';
+import { DbShipment, DbShipmentStatus } from '../../../types/aliases';
 import { PageContainer } from './index.styles';
+import { StatusCard } from '../../../components/common/status-card';
+import { IconShipmentStatusPlanned } from '../../../assets/icons/shipments';
 
 interface ShipmentViewPageProps {
   shipmentService: ShipmentService;
 }
 
-// TODO grab shipmentID from param url
 export const ShipmentViewPage: React.FC<ShipmentViewPageProps> = ({ shipmentService }) => {
   const { id } = useParams<{ id: string }>();
   const [shipment, setShipment] = useState<DbShipment | null>(null);
+  const [shipmentStatuses, setShipmentStatuses] = useState<DbShipmentStatus[]>([]);
   const [errors, setErrors] = useState<String[]>([]);
 
   useEffect(() => {
@@ -21,8 +23,23 @@ export const ShipmentViewPage: React.FC<ShipmentViewPageProps> = ({ shipmentServ
       if (data != null) setShipment(data);
       if (error != null) setErrors((prevErrors) => [...prevErrors, error.message]);
     };
+
+    const loadShipmentStatuses = async () => {
+      const { data, error } = await shipmentService.fetchShipmentStatuses();
+
+      if (data != null) setShipmentStatuses(data);
+      if (error != null) setErrors((prevErrors) => [...prevErrors, error.message]);
+    };
+
     loadShipment();
+    loadShipmentStatuses();
   }, [id, shipmentService]);
+
+  const defaultStatusCard = <StatusCard icon={IconShipmentStatusPlanned} field="Shipment Status" status="Loading..."></StatusCard>;
+  const resolveStatus = (shipmentStatusId: DbShipmentStatus['id']) => {
+    const maybeStatus = shipmentStatuses.find((status) => status.id == shipmentStatusId)?.status;
+    return maybeStatus !== undefined ? maybeStatus : 'Unknown';
+  };
 
   return (
     <>
@@ -30,10 +47,12 @@ export const ShipmentViewPage: React.FC<ShipmentViewPageProps> = ({ shipmentServ
         // TODO #15 make an error component and pass error in as props
         errors.map((error, index) => <p key={index}>{error}</p>)
       ) : (
-        // TODO: shipment list component
         <PageContainer>
-          {/* TODO insert components here */}
-          <p>{shipment ? JSON.stringify(shipment) : 'Loading...'}</p>
+          {shipment !== null ? (
+            <StatusCard icon={IconShipmentStatusPlanned} field="Shipment Status" status={resolveStatus(shipment.status_id)}></StatusCard>
+          ) : (
+            defaultStatusCard
+          )}
         </PageContainer>
       )}
     </>
